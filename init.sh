6a3ad5b128509d4f2bd0000dab6df446dfe71d4e
PeRDy <perdy.hh@gmail.com>
2015-06-11 18:01:42 +0200

    Fix chsh in init.sh

--- init.sh
+++ init.sh
@@ -12,7 +12,7 @@ sudo apt-get install git -y &>/dev/null
 echo "Install zsh"
 sudo apt-get install zsh -y &>/dev/null
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - &>/dev/null | sh &>/dev/null
-chsh -s /bin/zsh `whoami`
+sudo chsh -s /bin/zsh `whoami`
 
 # GVim
 echo "Install GVim"
